dfaacee56d58ba1fd380fa927258389d270bfcb6
thezenmaster <krasimir.nikolov@fdiba.tu-sofia.bg>
2013-05-05 14:46:46 +0300

    Initial commit on Input Parser and DB Scheme

---
+++ AuthorPaperIdentification/InputParser.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data.Entity.Validation;
+
+namespace AuthorPaperIdentification
+{
+    class InputParser
+    {
+        static void Main(string[] args)
+        {
+            var context = new AuthorPaperEntities();
+            context.Configuration.AutoDetectChangesEnabled = false;
+            //string[] files = { "Author", "Conference", "Journal", "Paper", "PaperAuthor", "Train", "Valid" };
+            string[] files = { "Conference", "Journal", "Paper" };
+            foreach (var item in files)
+            {
+                var filePath = @"..\..\Input\" + item + ".csv";
+                if (File.Exists(filePath))
+                {
+                    using (StreamReader reader = new StreamReader(filePath))
+                    {
+                        var startTime = DateTime.Now;
+                        switch (item)
+                        {
+                            case "Author":                                
+                                PopulateAuthors(ref context, reader);
+                                break;
+                            case "Paper":
+                                PopulatePapers(ref context, reader);
+                                break;
+                            case "Conference":
+                                PopulateConferences(ref context, reader);
+                                break;
+                            case "Journal":
+                                PopulateJournals(ref context, reader);
+                                break;
+                        }
+
+                        var endTime = DateTime.Now;
+                        Console.WriteLine(endTime - startTime);
+                        Console.ReadLine();
+                    }
+                }
+            }
+
+        }
+
+        private static void PopulateConferences(ref AuthorPaperEntities context, StreamReader reader)
+        {
+            //Skip the first line that contains column names
+            var line = reader.ReadLine();
+            int i = 0;
+            while (reader.Peek() >= 0)
+            {
+                line = reader.ReadLine();
+                var values = line.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                if (values.Length < 3)
+                    continue;
+                try
+                {
+                    var id = int.Parse(values[0]);
+                    if (!context.Conferences.Any(c => c.Id == id))
+                    {
+                        i++;
+                        var conference = new Conference();
+                        conference.Id = id;
+                        conference.ShortName = values[1].Trim(new char[] { '"' });
+                        conference.FullName = values[2].Trim(new char[] { '"' });
+                        if (values.Length == 4)
+                        {
+                            var homePage = values[3].Trim(new char[] { '"' });
+                            if (!String.IsNullOrEmpty(homePage))
+                                conference.HomePage = homePage;
+                        }
+                        context.Conferences.Add(conference);
+                        if (i == 1000)
+                        {
+                            i = 0;
+                            context.SaveChanges();
+                            context.Dispose();
+                            context = new AuthorPaperEntities();
+                            context.Configuration.AutoDetectChangesEnabled = false;
+                        }
+                    }
+                }
+                catch (DbEntityValidationException dbEx)
+                {
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+
+                        }
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+
+        private static void PopulateJournals(ref AuthorPaperEntities context, StreamReader reader)
+        {
+            //Skip the first line that contains column names
+            var line = reader.ReadLine();
+            int i = 0;
+            while (reader.Peek() >= 0)
+            {
+                line = reader.ReadLine();
+                var values = line.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                if (values.Length < 3)
+                    continue;
+                try
+                {
+                    var id = int.Parse(values[0]);
+                    if (!context.Journals.Any(j => j.Id == id))
+                    {
+                        i++;
+                        var journal = new Journal();
+                        journal.Id = id;
+                        journal.ShortName = values[1].Trim(new char[] { '"' });
+                        journal.FullName = values[2].Trim(new char[] { '"' });
+                        if (values.Length == 4)
+                        {
+                            var homePage = values[3].Trim(new char[] { '"' });
+                            if (!String.IsNullOrEmpty(homePage))
+                                journal.HomePage = homePage;
+                        }
+                        if (journal.ShortName == null)
+                        {
+
+                        }
+                        context.Journals.Add(journal);
+                        if (i == 1000)
+                        {
+                            i = 0;
+                            context.SaveChanges();
+                            context.Dispose();
+                            context = new AuthorPaperEntities();
+                            context.Configuration.AutoDetectChangesEnabled = false;
+                        }
+                    }
+                }
+                catch (DbEntityValidationException dbEx)
+                {
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                           
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+            }
+        }
+
+        private static void PopulateAuthors(ref AuthorPaperEntities context, StreamReader reader)
+        {
+            //Skip the first line that contains column names
+            var line = reader.ReadLine();
+            int i = 0;
+            while (reader.Peek() >= 0)
+            {
+                line = reader.ReadLine();
+                var values = line.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                if (values.Length < 2)
+                    continue;
+                try
+                {
+                    var id = int.Parse(values[0]);
+                    if (!context.Authors.Any(a => a.Id == id))
+                    {
+                        i++;
+                        var author = new Author();
+                        author.Id = id;
+                        author.Name = values[1];
+                        if (values.Length == 3)
+                            author.Affiliation = values[2];
+                        context.Authors.Add(author);
+                        if (i == 1000)
+                        {
+                            i = 0;
+                            context.SaveChanges();
+                            context.Dispose();
+                            context = new AuthorPaperEntities();
+                            context.Configuration.AutoDetectChangesEnabled = false;
+                        }
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+
+        private static void PopulatePapers(ref AuthorPaperEntities context, StreamReader reader)
+        {
+            //Skip the first line that contains column names
+            var line = reader.ReadLine();
+            int i = 0;
+            while (reader.Peek() >= 0)
+            {
+                line = reader.ReadLine();
+                var values = line.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                if (values.Length < 5)
+                    continue;
+                try
+                {
+                    var id = int.Parse(values[0]);
+                    if (!context.Papers.Any(p => p.Id == id))
+                    {
+                        i++;
+                        var paper = new Paper();
+                        paper.Id = id;
+                        paper.Title = values[1];
+                        paper.Year = int.Parse(values[2]);
+                        var conferenceId = int.Parse(values[3]);
+                        if (conferenceId > 0)
+                            paper.ConferenceId = conferenceId;
+                        var journalId = int.Parse(values[4]);
+                        if (journalId > 0)
+                            paper.JournalId = journalId;
+                        if (values.Length == 6)
+                            paper.Keywords = values[5];
+                        context.Papers.Add(paper);
+                        if (i == 1000)
+                        {
+                            i = 0;
+                            context.SaveChanges();
+                            context.Dispose();
+                            context = new AuthorPaperEntities();
+                            context.Configuration.AutoDetectChangesEnabled = false;
+                        }
+                    }
+                }
+                catch (DbEntityValidationException dbEx)
+                {
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
